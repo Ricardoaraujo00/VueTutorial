0d7beb6c7b58aa4506c87e28af459909df4210ae
Ricardo Araújo <ricardo.araujo.t0114627@edu.atec.pt>
2020-04-17 23:27:52 +0100

    Create a basic Vue.ja componet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -5,7 +7,7 @@
  */
 
 require('./bootstrap');
-require('./fontawesome');
+//require('./fontawesome');
 
 window.Vue = require('vue');
 
@@ -20,14 +22,15 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('user-info', () => import(/* webpackChunkName: "UserInfoComponent" */ './components/UserInfo.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+Vue.config.productionTip = false;
 const app = new Vue({
     el: '#app',
 });
+console.log("Vue krlh", app);
--- resources/js/fontawesome.js
+++ resources/js/fontawesome.js
@@ -1,4 +1,9 @@
-import fontawesome from '@fortawesome/fontawesome-free';
+// import Vue from 'vue'
+// import { library } from '@fortawesome/fontawesome-free';
+// import { faCaretUp,faCaretDown,faStar,faCheck } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+import fontawesome from '@fortawesome/fontawesome';
 import faCaretUp from '@fortawesome/free-solid-svg-icons/faCaretUp';
 import faCaretDown from '@fortawesome/free-solid-svg-icons/faCaretDown';
 import faStar from '@fortawesome/free-solid-svg-icons/faStar';
